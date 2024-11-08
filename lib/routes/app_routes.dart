b6d5c8ddc55b5f43e2850d06781f0eaa5329a1f4
Pedro Bosch <pedro@andared.com>
2024-11-08 09:53:18 +0100

    MenuOption personalizado (Primer elemento)

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,18 @@
+
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final MenuOptions = <MenuOption>[
+    MenuOption(
+      route: 'home', 
+      icon: Icons.house_rounded, 
+      name: 'Home Screen', 
+      screen: const HomeScreen())
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
         'listview1': (BuildContext context) => const Listview1Screen(),
