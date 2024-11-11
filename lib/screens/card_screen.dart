089bc1cc09da5962bad0d77a99efba2d2fa81190
Pedro Bosch <pedro@andared.com>
2024-11-11 12:03:31 +0100

    CustomCardTipo1

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,4 +1,4 @@
-import 'package:fl_components/theme/app_theme.dart';
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -14,20 +14,10 @@ class CardScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.symmetric( horizontal: 20, vertical: 10),
         children: [
-          Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary),
-                  title: Text('Título de la tarjeta'),
-                  subtitle: Text('Ea duis aute cillum ipsum duis duis ex nostrud ad irure cillum. Aliquip cupidatat eiusmod magna elit excepteur ullamco. Tempor et sunt officia deserunt excepteur. In esse duis sint consequat est magna fugiat dolore. Magna dolor laborum sint elit quis nisi tempor occaecat. Ex exercitation amet proident ut ipsum eiusmod reprehenderit reprehenderit duis. Consectetur ex nulla nulla minim velit cupidatat sit.'),
-                )
-
-              ],
-            ),
-          )
+          CustomCardTipo1()
         ],
       )
     );
   }
 }
+
---
+++ lib/widgets/custom_card_tipo_1.dart
@@ -0,0 +1,24 @@
+import 'package:fl_components/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomCardTipo1 extends StatelessWidget {
+  const CustomCardTipo1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+            title: Text('Título de la tarjeta'),
+            subtitle: Text('Ea duis aute cillum ipsum duis duis ex nostrud ad irure cillum. Aliquip cupidatat eiusmod magna elit excepteur ullamco. Tempor et sunt officia deserunt excepteur. In esse duis sint consequat est magna fugiat dolore. Magna dolor laborum sint elit quis nisi tempor occaecat. Ex exercitation amet proident ut ipsum eiusmod reprehenderit reprehenderit duis. Consectetur ex nulla nulla minim velit cupidatat sit.'),
+          )
+    
+        ],
+      ),
+    );
+  }
+}
