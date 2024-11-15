0c571296b752082c727ffd55c157b754dff33c4d
Pedro Bosch <pedro@andared.com>
2024-11-15 09:15:43 +0100

    BorderRadius

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -6,10 +6,25 @@ class CustomCardTipo2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10)
+      ),
       child: Column(
         children: [
-          Image(
-            image: NetworkImage('https://images8.alphacoders.com/110/1103710.jpg')
+          const FadeInImage(
+            image: NetworkImage('https://images8.alphacoders.com/110/1103710.jpg'), 
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 260,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 3000),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only( top: 10, bottom: 10, right: 20),
+            child: 
+            const Text('The Mandalorian')
           )
         ],
       ),
